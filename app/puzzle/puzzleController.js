58213ee580f4d3f281ccd82ae530bf2159c5fb8a
Thomas Hibbard <thibbard@criticalmass.com>
2016-08-28 16:34:01 -0500

    chunking rows

--- app/puzzle/puzzleController.js
+++ app/puzzle/puzzleController.js
@@ -4,5 +4,18 @@ var app = angular.module('crosswordApp')
   crosswordCtrl.$inject = ['$scope', 'CrosswordService'];
 
 function crosswordCtrl($scope, CrosswordService){
- console.log(CrosswordService.getCrosswordData());
+	$scope.crosswordData = CrosswordService.getCrosswordData();
+	$scope.crosswordSize = $scope.crosswordData.size;
+	splitRows();
+
+	function splitRows(){
+		$scope.crosswordGrid = $scope.crosswordData.grid.map(function(item){
+			return item === '.' ? false : item;
+		});
+		$scope.rowChunk = _.chunk($scope.crosswordGrid, $scope.crosswordSize.cols);
+		console.log($scope.rowChunk)
+
+	}
+
+	console.log($scope.crosswordData);
 }

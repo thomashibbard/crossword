56ce8daa8aac5de1678020484ac3fd0429ff4cdc
Thomas Hibbard <thibbard@criticalmass.com>
2016-08-28 17:19:29 -0500

    work on adding numbers to each box

--- app/puzzle/puzzleController.js
+++ app/puzzle/puzzleController.js
@@ -6,16 +6,24 @@ var app = angular.module('crosswordApp')
 function crosswordCtrl($scope, CrosswordService){
 	$scope.crosswordData = CrosswordService.getCrosswordData();
 	$scope.crosswordSize = $scope.crosswordData.size;
+	$scope.crosswordGrid = $scope.crosswordData.grid.map(function(item){
+		return item === '.' ? false : item;
+	});
+	console.log($scope.crosswordGridnums);
+	$scope.crosswordGridnums = $scope.crosswordData.gridnums.map(function(item){
+		//console.log(item)
+		return item === 0 ? false : item;
+	});
+
 	splitRows();
 
 	function splitRows(){
-		$scope.crosswordGrid = $scope.crosswordData.grid.map(function(item){
-			return item === '.' ? false : item;
-		});
 		$scope.rowChunk = _.chunk($scope.crosswordGrid, $scope.crosswordSize.cols);
-		console.log($scope.rowChunk)
-
 	}
 
-	console.log($scope.crosswordData);
+	$scope.highlightVector = function($event){
+		console.log($event);
+	}
+
+	//console.log($scope.crosswordData);
 }

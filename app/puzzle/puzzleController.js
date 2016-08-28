63644b80fdbaf4ffec198945f38b60d410abf7ac
Thomas Hibbard <thibbard@criticalmass.com>
2016-08-28 18:00:23 -0500

    adding logic to properly number clues

--- app/puzzle/puzzleController.js
+++ app/puzzle/puzzleController.js
@@ -6,14 +6,16 @@ var app = angular.module('crosswordApp')
 function crosswordCtrl($scope, CrosswordService){
 	$scope.crosswordData = CrosswordService.getCrosswordData();
 	$scope.crosswordSize = $scope.crosswordData.size;
+
 	$scope.crosswordGrid = $scope.crosswordData.grid.map(function(item){
 		return item === '.' ? false : item;
 	});
-	console.log($scope.crosswordGridnums);
+
 	$scope.crosswordGridnums = $scope.crosswordData.gridnums.map(function(item){
-		//console.log(item)
+		// console.log(item)
 		return item === 0 ? false : item;
 	});
+	//console.log($scope.crosswordData)
 
 	splitRows();
 
@@ -22,8 +24,19 @@ function crosswordCtrl($scope, CrosswordService){
 	}
 
 	$scope.highlightVector = function($event){
-		console.log($event);
+		//console.log($event);
 	}
-
+	console.log($scope.crosswordGridnums);
+	$scope.getClueId = function(){
+		//console.log(arguments);
+	}
+	$scope.getCanonicalIndex = function(rowIndex, squareIndex){
+		//console.log((rowIndex)*$scope.crosswordSize.rows, squareIndex+1, $scope.crosswordSize);
+		console.log($scope.crosswordGridnums);
+		var row = rowIndex * $scope.crosswordSize.rows;
+		console.log(row + squareIndex)
+		return row + squareIndex;
+		//(rowIndex+rowChunk.length)+(squareIndex)
+	}
 	//console.log($scope.crosswordData);
 }

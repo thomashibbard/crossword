f54085a2ff27b3bae588fa5e01080e597926a65d
Thomas Hibbard <thibbard@criticalmass.com>
2016-08-28 18:50:05 -0500

    adding functionality to select active square

--- app/puzzle/puzzleController.js
+++ app/puzzle/puzzleController.js
@@ -6,35 +6,47 @@ var app = angular.module('crosswordApp')
 function crosswordCtrl($scope, CrosswordService){
 	$scope.crosswordData = CrosswordService.getCrosswordData();
 	$scope.crosswordSize = $scope.crosswordData.size;
+	$scope.activeSquareId = 1;
+	$scope.activeVectorId = 'a';
+	$scope.activeSquareIndex = 0;
+
 
 	$scope.crosswordGrid = $scope.crosswordData.grid.map(function(item){
 		return item === '.' ? false : item;
 	});
 
 	$scope.crosswordGridnums = $scope.crosswordData.gridnums.map(function(item){
-		// console.log(item)
 		return item === 0 ? false : item;
 	});
-	//console.log($scope.crosswordData)
 
 	splitRows();
 
 	function splitRows(){
 		$scope.rowChunk = _.chunk($scope.crosswordGrid, $scope.crosswordSize.cols);
 	}
+	console.log($scope.crosswordData)
 
-	$scope.highlightVector = function($event){
-		//console.log($event);
-	}
-	console.log($scope.crosswordGridnums);
-	$scope.getClueId = function(){
+	$scope.selectClue = function(canonicalIndex, rowIndex, squareIndex){
+		highlightVector(canonicalIndex, rowIndex, squareIndex);
+		displayClue(canonicalIndex, rowIndex, squareIndex);
+
+	};
+
+	function displayClue (canonicalIndex, rowIndex, squareIndex){
+		$scope.activeSquareIndex = canonicalIndex;
+	};
+
+	function highlightVector  (canonicalIndex, rowIndex, squareIndex){
+		console.log('in here thomas')
+	};
+
+	function getClueId(){
 		//console.log(arguments);
 	}
+
 	$scope.getCanonicalIndex = function(rowIndex, squareIndex){
 		//console.log((rowIndex)*$scope.crosswordSize.rows, squareIndex+1, $scope.crosswordSize);
-		console.log($scope.crosswordGridnums);
 		var row = rowIndex * $scope.crosswordSize.rows;
-		console.log(row + squareIndex)
 		return row + squareIndex;
 		//(rowIndex+rowChunk.length)+(squareIndex)
 	}
